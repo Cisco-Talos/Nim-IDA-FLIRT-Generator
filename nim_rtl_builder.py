71133ce3f94a0dc6c06030aca15b3b0b2a44cad1
Holger Unterbrink <hunterbr@cisco.com>
2023-08-17 19:10:23 +0200

    minor changes to README

--- nim_rtl_builder.py
+++ nim_rtl_builder.py
@@ -7,16 +7,31 @@
 # includes these functions and their functions calls.                             #                                                     
 #                                                                                 # 
 # Author: Holger Unterbrink (hunterbr@cisco.com)                                  #                                                
-#                                                                                 # 
-# (c) 2023 Cisco Talos / Primary License: BSD (except code from external sources) #                                                                                  
-#                                                                                 # 
+#                                                                                 #
 # IMPORTANT:                                                                      # 
 # Run this script in the '<Nim_install_dir>\lib\pure' directory and make sure     #                                                                             
 # the 'nim' executable is in your PATH                                            #                                      
 #                                                                                 # 
 # Usage: nim_rtl_builder.py                                                       # 
 #                                                                                 # 
-################################################################################### 
+#                                                                                 #
+# Copyright 2022 Cisco Systems, Inc. and its affiliates                           #
+#                                                                                 #
+# Licensed under the Apache License, Version 2.0 (the "License");                 #
+# you may not use this file except in compliance with the License.                #
+# You may obtain a copy of the License at                                         #
+#                                                                                 #
+#      http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                                 #
+# Unless required by applicable law or agreed to in writing, software             #
+# distributed under the License is distributed on an "AS IS" BASIS,               #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.        #
+# See the License for the specific language governing permissions and             #
+# limitations under the License.                                                  #
+#                                                                                 #
+# SPDX-License-Identifier: Apache-2.0                                             #
+#                                                                                 #
+###################################################################################
 
 import sys
 import re
